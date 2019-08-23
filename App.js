f4ee30e3e813eb92b96a0da72d6a11f2ee5a82e4
Henry <henrygraves95@gmail.com>
2019-08-23 09:46:52 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
+
+const navigator = createStackNavigator({
+    Index: IndexScreen,
+  }, 
+  {
+    initalRoutName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blogs'
+    }
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+  <BlogProvider>
+    <App />
+  </BlogProvider>
+  )
+}
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Text, StyleSheet, Button } from 'react-native';
+import BlogContext from '../context/BlogContext';
+import { FlatList } from 'react-native-gesture-handler';
+
+const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
+  return (
+    <>
+      <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost}/>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (<Text>{item.title}</Text>)
+        }}
+      />
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IndexScreen;

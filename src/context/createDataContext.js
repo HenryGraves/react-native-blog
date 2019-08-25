18b78490874aacd8ac8c913e0bdc6f02bf271267
Henry <henrygraves95@gmail.com>
2019-08-24 17:24:05 -0700

    automating context

---
+++ src/context/createDataContext.js
@@ -0,0 +1,8 @@
+import React, { useReducer } from 'react';
+
+export default (reducer, actions, initialState) => {
+  const Context = React.createContext();
+  const Provider = ({ children }) => {
+    
+  }
+}
